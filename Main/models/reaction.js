ea10ae548dceee5d3caf645956d96e8fff274253
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2024-02-03 13:44:10 -0600

    created reaction models

---
+++ Main/models/reaction.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+ 
+
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+
+  username: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now, // Set default value to the current timestamp
+    get: (createdAt) => {
+      // Use a getter method to format the timestamp on query
+      return new Date(createdAt).toLocaleString(); 
+    },
+  },
+});
+
+const Model = mongoose.model('Model', schema);
+
+module.exports = Model;
